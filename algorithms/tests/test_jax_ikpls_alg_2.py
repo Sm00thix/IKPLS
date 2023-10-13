440b000aa0767754bb7b871c699c412b37a058a5
Ole Engstrøm <oleemail@icloud.com>
2023-10-13 21:49:36 +0200

    Checking that JAX IKPLS Alg 2 can fit and predict

---
+++ algorithms/tests/test_jax_ikpls_alg_2.py
@@ -0,0 +1,10 @@
+from algorithms.jax_ikpls_alg_2 import PLS
+import jax.numpy as jnp
+import numpy as np
+
+def test_fit_predict():
+    X = jnp.array(np.random.uniform(size=(10,10)))
+    Y = jnp.array(np.random.uniform(size=(10,2)))
+    p = PLS()
+    p.fit(X, Y, 5)
+    p.predict(X)
