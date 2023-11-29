db4bbcf954249b85cbba665c2a62d690f12e6298
Ole-Christian Galbo Engstrøm <oleemail@icloud.com>
2023-11-29 16:41:59 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ autosummary_generate = True
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Options for LaTeX output ------------------------------------------------
