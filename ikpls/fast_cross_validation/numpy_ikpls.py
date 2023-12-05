435dc3d646517b2da51360ff268e46f5556a4179
Sm00thix <oleemail@icloud.com>
2023-12-05 19:20:43 +0100

    Updated docstrings

--- ikpls/fast_cross_validation/numpy_ikpls.py
+++ ikpls/fast_cross_validation/numpy_ikpls.py
@@ -10,7 +10,7 @@ from joblib import Parallel, delayed
 
 class PLS:
     """
-    Implements partial least-squares regression using Improved Kernel PLS by Dayal and MacGregor: https://doi.org/10.1002/(SICI)1099-128X(199701)11:1%3C73::AID-CEM435%3E3.0.CO;2-%23
+    Implements fast cross-validation with partial least-squares regression using Improved Kernel PLS by Dayal and MacGregor: https://doi.org/10.1002/(SICI)1099-128X(199701)11:1%3C73::AID-CEM435%3E3.0.CO;2-%23
 
     Parameters
     ----------
